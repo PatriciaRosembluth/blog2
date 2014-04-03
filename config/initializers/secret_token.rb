3abb74f9236dfc26a7af1aac91259fca22a56cc7
Patricia Rosembluth Prado <patty_rosembluth@hotmail.com>
2014-04-03 11:54:02 -0400

    Creacion del repositorio en git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'd8f5a66f75b5d1f87c884a5c0c825cfeefc270d5ebb772607ac225e59d5a57232bf4125999f0c9bd86720bed5401e3af274934407f74d60747b77c93a06817dd'
